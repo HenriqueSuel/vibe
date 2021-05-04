160f19d4043e2ed8352cbb89901f462f14ef4fdb
Henrique Suel <h.suel17@hotmail.com>
2021-05-04 11:51:45 -0300

    teste action 16

--- tests/unit/views/login.spec.js
+++ tests/unit/views/login.spec.js
@@ -53,4 +53,86 @@ describe('Login.vue', () => {
     wrapper.vm.$v.$touch();
     expect(wrapper.html()).toMatchSnapshot();
   });
+
+  it('should login in with the patient', async () => {
+    const wrapper = shallowMount(Login, {
+      stubs: ['router-link'],
+      directives: {
+        mask: VueMaskDirective,
+      },
+      localVue,
+      store,
+    });
+    wrapper.setData({ isPatient: true, doc: '460.978.918-30' });
+    const buttonSubmit = wrapper.find('button');
+    buttonSubmit.trigger('submit');
+    await flushPromises();
+    expect(actions.actionPatientLogin).toHaveBeenCalled();
+    expect(actions.actionToast).not.toHaveBeenCalled();
+  });
+
+  it('shouldnt login in with the patient', async () => {
+    actions = {
+      actionPatientLogin: jest.fn().mockResolvedValue(null),
+      actionToast: jest.fn().mockResolvedValue(),
+    };
+
+    store = new Vuex.Store({
+      state: {},
+      modules: {
+        PatientStore: {
+          namespaced: true,
+          actions,
+        },
+        GlobalStore: {
+          namespaced: true,
+          actions,
+        },
+      },
+    });
+    const wrapper = shallowMount(Login, {
+      stubs: ['router-link'],
+      directives: {
+        mask: VueMaskDirective,
+      },
+      localVue,
+      store,
+    });
+    wrapper.setData({ isPatient: true, doc: '460.978.918-30' });
+    const buttonSubmit = wrapper.find('button');
+    buttonSubmit.trigger('submit');
+    await flushPromises();
+    expect(actions.actionPatientLogin).toHaveBeenCalled();
+    expect(actions.actionToast).toHaveBeenCalled();
+  });
+
+  it('After the user submit button, $v.$invalid return true?', () => {
+    const wrapper = shallowMount(Login, {
+      stubs: ['router-link'],
+      directives: {
+        mask: VueMaskDirective,
+      },
+      localVue,
+      store,
+    });
+    const loginBtn = wrapper.find('button');
+    loginBtn.trigger('submit');
+    expect(wrapper.vm.$v.$invalid).toBeTruthy();
+  });
+
+  it('should log in with the doctor', async () => {
+    const wrapper = shallowMount(Login, {
+      stubs: ['router-link'],
+      directives: {
+        mask: VueMaskDirective,
+      },
+      localVue,
+      store,
+    });
+    wrapper.setData({ isPatient: false, doc: '46097891830' });
+    const buttonSubmit = wrapper.find('button');
+    buttonSubmit.trigger('submit');
+    await flushPromises();
+    expect(actions.actionDoctorLogin).toHaveBeenCalled();
+  });
 });
